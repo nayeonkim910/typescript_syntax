84dc15f5df847484ec8673b433d6b33bc23206e3
nayeonkim910 <yoni91059105@gmail.com>
2023-01-17 13:22:46 +0900

    stack

--- 3-OOP/3-5-inheritance.ts
+++ 3-OOP/3-5-inheritance.ts
@@ -7,132 +7,91 @@
  */
     type CoffeeCup = {
         shots: number;
-        hasMilk?: boolean;
+        hasMilk: boolean;
         syrup?:boolean;
     }
 
     interface CoffeeMaker {
         makeCoffee(shots: number): CoffeeCup;
     }
-    //Syrup interface
-    interface SyrupPump {
-        addSyrup(coffee:CoffeeCup):CoffeeCup;
-    }
-    //Milk interface
-    interface MilkFrother {
-        steamMilk(coffee:CoffeeCup):CoffeeCup;
-    }
-//--------------------------------
-    class milkSteamer implements MilkFrother{
-        private steam(){
-            console.log('-------------steaming milk......🥛');
-        }
-        steamMilk(coffee:CoffeeCup):CoffeeCup{
-            this.steam();
-            return {...coffee,hasMilk:true};
-        }
-    }
 
-    class noMilk implements MilkFrother{
-        steamMilk(coffee:CoffeeCup):CoffeeCup{
-            return {...coffee,hasMilk:false};
-        }
-    }
-//--------------------------------
-
-    class syrupAdder implements SyrupPump{
-        private pumpSyrup(){
-            console.log('--------------pump Syrup...🍶');
-        }
-        addSyrup(coffee:CoffeeCup):CoffeeCup{
-            this.pumpSyrup();
-            return {...coffee,syrup:true};
-        }
-    }
-    class noSyrup implements SyrupPump{
-        addSyrup(coffee:CoffeeCup){
-            return {...coffee,syrup:false};
-        }
-    }
-    class PremiumsyrupPump implements SyrupPump{
-        private pumpSyrup(){
-            console.log('pump  Premium Syrup...🍶💚❤💘😉');
-        }
-        addSyrup(coffee:CoffeeCup):CoffeeCup{
-            this.pumpSyrup();
-            return {...coffee,syrup:true};
-        }
-    }
-//--------------------------------
     class CoffeeMachine implements CoffeeMaker {
         //static은 상수목적이라 변경할 일 없음 
-        private static BEANS_GRAMM: number = 7  
-        // private coffeeBeans!: number;
-            constructor(
-                private coffeeBeans: number,
-                private syrupDivice:SyrupPump,
-                private milkDevice:MilkFrother){ 
+        private static BEANS_GRAMM: number = 7 //static인데도 class레벨에서 사용불가.
+        private coffeeBeans!: number;
+
+         constructor(beans: number) { //GRAMM정해서 머신만들기
+            this.coffeeBeans = beans;
+        }
+        static makeCoffeeMachine(beans: number): CoffeeMachine {
+            return new CoffeeMachine(beans);
+        }
+        clean() {
+            console.log(`wasing---machine---🚿 `);
+        }
+        fillCoffeeBeans(beans: number) {
+            if (beans <= 0) {
+                throw new Error('value for beans should be greater than 0');
             }
-            fillCoffeeBeans(beans: number) {
-                if (beans <= 0) {
-                    throw new Error('value for beans should be greater than 0');
-                }
-                this.coffeeBeans += beans;
+            this.coffeeBeans += beans;
+        }
+
+        private grind(shots: number) {
+            console.log(`grinding...for coffee ☕`);
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM;
+            if (this.coffeeBeans < 0) {
+                throw new Error('value for beans should be greater than 0');
             }
-            private grind(shots: number) {
-                console.log(`grinding...for coffee ☕`);
-                this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM;
-                if (this.coffeeBeans < 0) {
-                    throw new Error('value for beans should be greater than 0');
-                }
+        }
+        private heating() {
+            console.log(`heating now...♨️`);
+        }
+
+        private extract(shots: number): CoffeeCup {
+            console.log(`extract one shot 🔴`);
+            return {
+                shots,
+                hasMilk: false,
             }
-            private heating() {
-                console.log(`heating now...♨️`);
-            }
-            private extract(shots: number): CoffeeCup {
-                console.log(`extract one shot 🔴`);
-                return {
-                    shots,
-                    hasMilk: false,
-                    syrup:false,
-                }
-            }
-            makeCoffee(shots: number): CoffeeCup {
-                this.grind(shots);
-                this.heating();
-                const coffee = this.extract(shots);
-                const hasSyrup=this.syrupDivice.addSyrup(coffee);
-                return this.milkDevice.steamMilk(hasSyrup);
-            }
+        }
+        makeCoffee(shots: number): CoffeeCup {
+            this.grind(shots);
+            this.heating();
+            return this.extract(shots);
+        }
     }
 
-    //milk 종류
-    const milk = new milkSteamer();
-    const notM = new noMilk();
+    class CaffeLatteMachine extends CoffeeMachine {
+        constructor(beans:number){
+            super(beans);
+        }
+        private steamMilk():void{
+            console.log(`🥛 Steaming some milk`);
+        }
+        makeCoffee(shots: number): CoffeeCup {
+            const coffee = super.makeCoffee(shots);
+            this.steamMilk();
+            return { ...coffee, hasMilk: true };
+            
+        }
+        
+    }
+         
+        
+    const Lmachine = new CaffeLatteMachine(50);
+    const latte= Lmachine.makeCoffee(2);
+    console.log(latte);
+  
+     
+     
     
-    //syrup 종류
-    const syrup = new syrupAdder();
-    const PremiumSyrup = new PremiumsyrupPump();
-    const notS = new noSyrup();
-    //CoffeeMachine클래스 하나를 가지고 다양한 기기를 ☑️ 조립하기
-    //extends 상속을 전혀 사용하지않았지만,원하는 형태의 오브젝트를 만들어냄.
-    //미리 interface로 약속해두고, 조건에 맞는 블럭으로 조립
-    const SWCFMaker= new CoffeeMachine(33,syrup,notM);
-    const SWLatteMaker = new CoffeeMachine(33,PremiumSyrup,milk);
-    const LatteMaker = new CoffeeMachine(33,notS,milk);
-
-    const machineSet:CoffeeMachine[]= [
-        SWCFMaker,
-        SWLatteMaker,
-        LatteMaker
-    ];
-
-    machineSet.forEach((MC)=>MC.makeCoffee(2));
+    
 }
-   
-   
-   
-   
-   
 
+
+    // const deepCoffeefmaker =CoffeeMachine.makeCoffeeMachine(50);
+    // const pro = new ProBarista(deepCoffeefmaker);
+    // const ama = new Amateur(deepCoffeefmaker);
+    // pro.makeCoffee();
+    // ama.makeCoffee();
 
--- 4-OOP-project/stack.ts
+++ 4-OOP-project/stack.ts
@@ -1,51 +1,58 @@
 {
-interface Stack{
-    readonly size : number;
-    push(value: string):void;
-    pop():string;
-}
-
-type StackNode= {
-   readonly value:string;
-   readonly next?: StackNode;
-}
-class StackImpl implements Stack{
-    private _size: number=0;
-    private head?:StackNode;
-
-    get size(){
-        return this._size;
+    //Stack 구조 만들기 
+    //Linked Array형싱
+    //interface는 계약서 같은 것으로 생각! 
+    interface Stack {
+        readonly size:number;
+        push(value:string):void;
+        pop():string;
     }
-    push(value:string){
-        const node:StackNode ={value, next: this.head}; 
-        this.head=node;
-        this._size++;
-        
+
+    type Node ={
+       readonly value:string;
+       readonly nextNode?:Node;  
+        //nextNode에 Node가 들어올 수도 있고, 
+        //undefined가 올 수 도 있음. 
     }
-    pop():string {
-        if(this.head==null){
-            throw new Error('Stack is empty !');
+
+    class StackImpl implements Stack {
+        private _size:number=0;
+        private head?:Node;
+        //외부에서 read만 가능하도록! getter생성.
+        get size(){ 
+            return this._size;
         }
-        const node = this.head;
-        this.head =node.next; 
-        this._size--;
-        return node.value;
-    }
+
+             push(value: string): void {
+                //node생성
+                 const node:Node = {
+                     value:value,
+                     nextNode: this.head,
+                 }
+                 this.head= node;
+                 this._size++;
+             }
+
+             pop(): string {                
+                 const node = this.head;
+                 if(node== null){
+                    throw new Error('head is null');
+                 }
+                 this.head= node.nextNode;
+                 this._size--;
+                 return node.value;
+             }
+
     
+    }
 
+    const STACK = new StackImpl();
+    STACK.push('처음');    
+    STACK.push('중간');    
+    STACK.push('마지막입장');
+    while(STACK.size>0){
+        console.log(STACK.pop());
+    }
+        
 
-}
- const Stack = new StackImpl();
- Stack.push('first');
-    console.log(Stack);
- Stack.push('second');
- console.log(Stack);
- Stack.push('third');
- console.log(Stack);
- Stack.push('forth');
- console.log(Stack);
- while(Stack.size!==0){
-     console.log(Stack.pop());
- }    
-   
 }
